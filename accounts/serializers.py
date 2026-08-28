78f2bc5962f65fbd6c2812eb54336251152e6ffb
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-08-28 03:30:10 +0330

    cleaned up serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,40 +3,13 @@ from django.utils.translation import gettext_lazy as _
 
 
 from .models import CustomUser
-from notifications.models import Notification
-from points.models import UserPoint
 
 class RegisterSerializer(serializers.ModelSerializer):
-    # password = serializers.CharField(max_length=128, write_only=True)
-    # password2 = serializers.CharField(max_length=128, write_only=True)
     class Meta:
         model = CustomUser
         fields = [
-            # 'first_name', changed
-            # 'last_name',  changed
-            # 'birth_date', changed
-            # 'gender', changed
-            # 'username', changed
-            # 'city', changed
             'phone_number', 
-            # 'email',  changed
-            # 'find',   changed
-            # 'password',  changed
-            # 'password2' changed
-            ]
-        
-    # def validate(self, attrs):
-    #     if attrs['password'] != attrs['password2']:
-    #         raise serializers.ValidationError({"password": _("رمز عبور و تکرار آن یکسان نیست.")})
-    #     return attrs
-        
-    # def create(self, validated_data):
-    #     password = validated_data.pop('password')
-    #     validated_data.pop('password2')
-    #     user = CustomUser.objects.create_user(**validated_data)
-    #     user.set_password(password)
-    #     user.save()
-    #     return user
+        ]    
 
 class SendOTPSerializer(serializers.Serializer):
     phone_number = serializers.CharField(max_length=11)
@@ -72,16 +45,6 @@ class UserSerializer(serializers.ModelSerializer):
             'find',
             'join',
         ]
-        
-class DashboardNotificationSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Notification
-        fields = ["id", 'title',"message", "is_read"]
-
-class DashboardPointsSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = UserPoint
-        fields = ['points', 'created_at']
 
 class ChangePasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
@@ -106,6 +69,6 @@ class ResetPasswordSerializer(serializers.Serializer):
         new_password_confirm = attrs.get('new_password_confirm')
 
         if new_password != new_password_confirm:
-            raise serializers.ValidationError({'error': 'رمز عبور و تکرار آن برابر نیستند.'})
+            raise serializers.ValidationError({'error': _('Your password and repeat does not match!')})
 
         return attrs
