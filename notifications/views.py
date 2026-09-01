bcb0f83076e915dc90b63e6c4be091d1e8343150
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-09-01 23:59:01 +0330

    bugfixed order for decorators notifications

--- notifications/views.py
+++ notifications/views.py
@@ -5,15 +5,15 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Notification
 from .serializers import NotificationSerializer
 
-@permission_classes([IsAuthenticated])
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def notification_list_view(request):
     notification_queryset = Notification.objects.filter(user=request.user).order_by('-created_at')
     serializer = NotificationSerializer(notification_queryset, many=True)
     return Response(serializer.data)
 
-@permission_classes([IsAuthenticated])
 @api_view(['PATCH'])
+@permission_classes([IsAuthenticated])
 def mark_notifications_read_view(request, pk):
     notification = get_object_or_404(Notification, pk=pk, user=request.user)
     updated = NotificationSerializer(notification ,data=request.data, partial=True)
