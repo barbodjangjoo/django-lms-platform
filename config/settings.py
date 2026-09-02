7fb9118c6949cd93c750a3b49558b4651b3026d2
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-09-02 23:14:04 +0330

    add celery broker

--- config/settings.py
+++ config/settings.py
@@ -168,3 +168,9 @@ JAZZMIN_SETTINGS = {
                             ],
     "theme": "darkly",
 }
+
+
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
