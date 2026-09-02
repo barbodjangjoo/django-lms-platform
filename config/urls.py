b49222b1e37bcbcebf7ab8f0824008f5f6e74a16
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-09-02 23:07:14 +0330

    add course urls

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/accounts/', include('accounts.urls')),
-    path('api/v1/notifications/', include('notifications.urls'))
+    path('api/v1/notifications/', include('notifications.urls')),
+    path('api/v1/course/', include('course.urls'))
 ]
