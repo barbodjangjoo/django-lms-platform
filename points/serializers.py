d0057deb5b636eee0353307bd4d95125708f7920
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-09-05 23:17:12 +0330

    remove challenges serializers

--- points/serializers.py
+++ points/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from points.models import UserPoint
-from challenge.models import UserMissionStatus
-from challenge import models as challenges
+
 
 class StageSerializer(serializers.ModelSerializer):
     first_name = serializers.SerializerMethodField()
@@ -22,32 +21,3 @@ class UserPointSerializer(serializers.ModelSerializer):
         user_point =  UserPoint.objects.filter(user=obj.user).all()
         return sum(point.points for point in user_point)    
 
-class UserMissionPointsSerializer(serializers.ModelSerializer):
-    activity_type = serializers.SerializerMethodField()
-    points = serializers.SerializerMethodField()
-    activity_id = serializers.SerializerMethodField()
-    created_at = serializers.DateTimeField(source="datetime_modified",read_only=True)
-    details = serializers.SerializerMethodField()
-    class Meta:
-        model = UserMissionStatus
-        fields = [
-            'id',
-            'user',
-            'points',
-            'activity_type',
-            "activity_id",
-            'details',
-            'created_at',
-        ]
-    
-    def get_activity_type(self, obj):
-        return "CHALLENGE"
-    
-    def get_activity_id(self, obj):
-        return obj.id
-    
-    def get_points(self, obj):
-        return obj.admin_points 
-    
-    def get_details(self, obj):
-        return f'امتیاز برای چلنج {obj.challenge.title} کسب شده است!'
